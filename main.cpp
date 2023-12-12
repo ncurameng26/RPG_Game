6860c2a20e21ad9db02b2e25a44961ba61f38153
nathancurameng <nathan.curameng@gmail.com>
2023-12-12 12:00:10 -0500

    Tested with different classes and changed formatting for print

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 
 int main() {
 
-    PlayerCharacter p1(new Cleric());
+//    PlayerCharacter p1(new Cleric());
+    PlayerCharacter p1( new Warrior());
 
 
     for (int i =0; i < 2; i++){
@@ -15,12 +16,19 @@ int main() {
             <<  "Level: " <<p1.getLevel() << "\n"
             << "EXP: " <<p1.getCurrentEXP() << "/" << p1.getEXPToNextLevel() << '\n'
             << "HP: " << p1.getCurrentHP() << "/" <<  p1.getMaxHP() << '\n'
+            << "MP: " << p1.getCurrentMP() << "/" <<  p1.getMaxMP() << '\n'
+            << "Armor: " <<p1.getArmor() << "\n"
             << "Strength: " << p1.getStrength() << "\n"
             << "Intellect: " <<p1.getIntellect() << "\n"
             << "Agility: " <<p1.getAgility() << "\n"
             << "Element Resistance: " <<p1.getElementResist() << "\n";
 
-        for(auto abil : p1.pc.Abilities)
+        auto AllAbilities = p1.getAbilityList();
+        std::cout << "Abilities: \n";
+
+        for(auto& abil : AllAbilities){
+            std::cout << "  -" << abil.Name <<'\n';
+        }
 
         if(i<1)
             p1.gainEXP(100u);
